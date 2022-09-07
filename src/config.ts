b8e8dcf175b405345b2cd328a544f55a260f1f74
kumpul4semut <semutkece6@gmail.com>
2022-09-07 08:12:56 +0700

    first setup blog use astro

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'kumpul4semut';
+export const SITE_DESCRIPTION = 'Selamat datang di website kumpul4semut';
